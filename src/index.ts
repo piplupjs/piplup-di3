50aa21cbfc81f567139554ed1ed7146a26b552c0
sadik-malik <malik0786sam@gmail.com>
2026-07-09 22:46:43 +0530

    feat: reexport getClassName and isClass utility

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { container } from './container.js';
 export { Inject } from './inject.js';
 export type { Constructor, FactoryProvider, InjectionToken } from './types.js';
+export { getClassName, isClass } from './utils.js';
